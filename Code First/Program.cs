c6ac8161b7e03179db30afb40f26d78071e30961
HkhCode <hkh.2003.1381@gmail.com>
2022-08-06 11:50:21 -0700

    File Upladed

---
+++ Code First/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+    public class dbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; } 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new dbContext())
+            {
+                var user = new User
+                {
+                    Name = "Hosein",
+                    Family = "Khadem"
+                };
+                db.Users.Add(user);
+                db.SaveChanges();
+                var selected = db.Users.FirstOrDefault(x => x.Name == "Hosein");
+                Console.WriteLine(selected.Id);
+                Console.WriteLine(selected.Name);
+                Console.WriteLine(selected.Family);
+                Console.WriteLine("====================");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
